eb770fe6be8ec7ac750cd17b842f153b6599bd56
Bhoomika <bhoomikahm18@gmail.com>
2023-08-16 14:38:22 +0530

    Set a time out to delay the result calculator

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ optionImages.forEach((image, index) => {
     image.addEventListener("click", (e) => {
         image.classList.add("active");
 
+        userResult.src = cpuResult.src = "images/rock.png";
+        result.textContent = "Wait...";
+
         //Loop through each image again
         optionImages.forEach((image2, index2) => {
             // console.log(index, index2);
@@ -19,45 +22,50 @@ optionImages.forEach((image, index) => {
             index !== index2 && image2.classList.remove("active");
         });
 
+        gameContainer.classList.add("start");
+        
+        //Set a time out to delay the result calculator
+        let time = setTimeout(() => {
+            gameContainer.classList.remove("start");
 
-        //Get the source of the clicked option image
-        let imageSrc = e.target.querySelector("img").src;
-        //Set the user image to the clicked option image
-        userResult.src = imageSrc;
-        // console.log(imageSrc);
+            //Get the source of the clicked option image
+            let imageSrc = e.target.querySelector("img").src;
+            //Set the user image to the clicked option image
+            userResult.src = imageSrc;
+            // console.log(imageSrc);
 
-        //Generate a random number between 0 and 2
-        let randomNumber = Math.floor(Math.random() * 3);
-        // console.log(randomNumber);
+            //Generate a random number between 0 and 2
+            let randomNumber = Math.floor(Math.random() * 3);
+            // console.log(randomNumber);
 
-        //Create an array of CPU image options
-        let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
-        cpuResult.src = cpuImages[randomNumber];
+            //Create an array of CPU image options
+            let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+            cpuResult.src = cpuImages[randomNumber];
 
-        //Assign a letter value to the CPU (R for Rock, P for Paper, S for Scissor)
-        let cpuValue = ["R", "P", "S"][randomNumber];
-        //Assign a letter value to the clicked option (based on index)
-        let userValue = ["R", "P", "S"][index];
+            //Assign a letter value to the CPU (R for Rock, P for Paper, S for Scissor)
+            let cpuValue = ["R", "P", "S"][randomNumber];
+            //Assign a letter value to the clicked option (based on index)
+            let userValue = ["R", "P", "S"][index];
 
-        //Create an object with all possible outcomes
-        let outComes = {
-            RR: "Draw",
-            RP: "CPU",
-            RS: "User",
-            PP: "Draw",
-            PR: "User",
-            PS: "CPU",
-            SS: "Draw",
-            SR: "CPU",
-            SP: "User"
-        };
+            //Create an object with all possible outcomes
+            let outComes = {
+                RR: "Draw",
+                RP: "CPU",
+                RS: "User",
+                PP: "Draw",
+                PR: "User",
+                PS: "CPU",
+                SS: "Draw",
+                SR: "CPU",
+                SP: "User"
+            };
 
-        //Look up the outcome value based on user and CPU options
-        let outComeValue = outComes[userValue + cpuValue];
+            //Look up the outcome value based on user and CPU options
+            let outComeValue = outComes[userValue + cpuValue];
 
-        //Display the result
-        result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!`;
-        // console.log(outComeValue);
-
+            //Display the result
+            result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!`;
+            // console.log(outComeValue);
+        }, 2500);
     });
 });
